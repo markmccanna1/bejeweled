26c5078192ce440b9adf57ac99b782a15a1f5ca4
Mark McCanna <markmccanna1@gmail.com>
2013-09-22 17:58:24 -0500

    game is mobile responsive

--- scripts/game.js
+++ scripts/game.js
@@ -19,9 +19,26 @@ jewel.game = (function() {
     dom.addClass(screen, 'active')
   }
 
+  //this setup function should be invoked once before the first screen is shown
+  function setup(){
+    //disable native touchmove behavior to prevent overscroll
+    dom.bind(document, 'touchmove', function(event) {
+      event.preventDefault()
+    })
+
+    //hide the address bar on android devices
+    if (/Android/.test(navigator.userAgent)) {
+      $('html')[0].style.height = '200%'
+      setTimeout(function() {
+        window.scrollTo(0,1)
+      }, 0)
+    }
+  }
+
   //expose public methods
   return {
-    showScreen : showScreen
+    showScreen : showScreen,
+    setup : setup
   }
 
 })()
--- scripts/loader.js
+++ scripts/loader.js
@@ -3,19 +3,56 @@ var jewel = {
 }
 
 window.addEventListener('load', function() {
+  
+  //this test determines whether the standalone mode is enabled
+  // because the question doesnt make senese on non-ios devices, we
+  // treat a non-existent standalone property as if its running in standalone mode
+
+  Modernizr.addTest('standalone', function() {
+    return (window.navigator.standalone != false)
+  })
+
+
+  //loading has been divided into 2 stages, the second stage is only activated if the game is running in 
+  // standalone mode
+  // stage 1
   Modernizr.load([
     {
-    load : ['scripts/sizzle.js',
+    load : [
+            'scripts/sizzle.js',
             'scripts/dom.js',
-            'scripts/game.js',
-            'scripts/screen.splash.js',
-            'scripts/screen.main-menu.js'],
-
-    //called when all files have finished loading and executing
-    complete: function() {
-      console.log('all files loaded')
-      jewel.game.showScreen('splash-screen')
-    }
+            'scripts/game.js'
+            // 'scripts/screen.splash.js',
+            // 'scripts/screen.main-menu.js'
+            ]
+    },
+    {
+      test: Modernizr.standalone,
+      yep: 'scripts/screen.splash.js',
+      nope: 'scripts/screen.install.js',
+      complete: function(){
+        jewel.game.setup()
+        if (Modernizr.standalone) {
+          jewel.game.showScreen('splash-screen')
+        } else {
+          jewel.game.showScreen('install-screen')
+        }
+      }
     }
   ])
+
+  // stage 2
+  if(Modernizr.standalone) {
+    Modernizr.load([
+    {
+      load: ['scripts/screen.main-menu.js']
+    }])
+  }
+    //called when all files have finished loading and executing
+  //   complete: function() {
+  //     console.log('all files loaded')
+  //     jewel.game.showScreen('splash-screen')
+  //   }
+  //   }
+  // ])
 }, false)
